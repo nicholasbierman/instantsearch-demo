1aaf5742d8b5dead7a7425db88267e0e118b973a
Nick Bierman <biermannick83@gmail.com>
2021-07-27 11:22:02 -0500

    add Snippet comp

--- instantsearch-app/src/components/Hit.js
+++ instantsearch-app/src/components/Hit.js
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { Highlight } from 'react-instantsearch-dom';
+import { Highlight, Snippet } from 'react-instantsearch-dom';
 
 export const Hit = ({ hit }) => {
   return (
     <div>
       <article>
         <img src={hit.image} alt={hit.name} width="100px" height="100px"></img>
+        <p>
+            {/* <code>{hit.name}</code>
+            <code>{hit.description}</code> */}
+        </p>
       </article>
       <Highlight hit={hit} attribute="name" tagName="code" />
       <br />
       <br />
-      <Highlight hit={hit} attribute="description" tagName="code" />
+      {/* <Highlight hit={hit} attribute="description" tagName="code" /> */}
+      <Snippet hit={hit} attribute="description" />
     </div>
   );
 };
