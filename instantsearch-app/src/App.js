27ac26ebc575fb31757c68c15feb8e6d44093460
Nick Bierman <biermannick83@gmail.com>
2021-08-03 18:28:05 -0500

    implement autocomplete

--- instantsearch-app/src/App.js
+++ instantsearch-app/src/App.js
@@ -1,88 +1,135 @@
+import React, { Component } from 'react';
 import algoliasearch from 'algoliasearch/lite';
-import React, { useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
 import {
-  ClearRefinements,
-  HierarchicalMenu,
-  Hits,
   InstantSearch,
   Configure,
-  Pagination,
+  Hits,
+  connectSearchBox,
+  connectRefinementList,
+  ClearRefinements,
+  HierarchicalMenu,
   RefinementList,
-  SearchBox,
+  Pagination,
   RatingMenu,
 } from 'react-instantsearch-dom';
+import Autocomplete from './components/Autocomplete';
+import Hit from './components/Hit';
 import './App.css';
-import HitWithInsights from './components/Hit';
+import { Route, Switch } from 'react-router-dom';
 import ProductDetails from './components/ProductDetails';
 import RelatedItem from './components/RelatedItem';
-import CustomAutocomplete from './components/Autocomplete';
-
 
 export const searchClient = algoliasearch(
   'NSMMHUZMQS',
   'ef0985fb06ac10d3b759ce42df2d4745'
 );
 
-function App() {
-  const [indexName, setIndexName] = useState('best-buy');
+const VirtualSearchBox = connectSearchBox(() => null);
+const VirtualRefinementList = connectRefinementList(() => null);
 
-  return (
-    <div className="container">
-      <Switch>
-        <Route path="/:product">
-          <ProductDetails />
-          
-          <RelatedItem />
-        </Route>
-        <Route path="/">
-          <select onChange={e => setIndexName(e.target.value)}>
-            <option value="best-buy">Relevance</option>
-            <option value="best-buy_price_desc">Price Descending</option>
-            <option value="best-buy-price_asc">Price Ascending</option>
-            <option value="best-buy_perso_nick">Personalized for me</option>
-          </select>
-          <InstantSearch searchClient={searchClient} indexName={indexName}>
-            <div className="search-panel">
-              <div className="search-panel__results">
-                <SearchBox
-                  className="searchbox"
-                  translations={{
-                    placeholder: 'Search for name, brand, type...',
-                  }}
+class App extends Component {
+  state = {
+    query: '',
+    categories: [],
+    indexName: "best-buy"
+  };
+
+  setIndexName = (e) => {
+    this.setState({
+      indexName: e.target.value
+    })
+  }
+
+  onSuggestionSelected = (_, { suggestion }) => {
+    const [
+      category,
+    ] = suggestion.instant_search.facets.exact_matches.categories;
+
+    this.setState({
+      query: suggestion.query,
+      categories:
+        category && category.value !== 'ALL_CATEGORIES' ? [category.value] : [],
+    });
+  };
+
+  onSuggestionCleared = () => {
+    this.setState({
+      query: '',
+      categories: [],
+    });
+  };
+
+  render() {
+    const { query, categories, indexName } = this.state;
+
+    return (
+      <div className="container">
+        <Switch>
+          <Route path="/:product">
+            <Route path="/:product">
+              <ProductDetails />
+
+              <RelatedItem />
+            </Route>
+          </Route>
+          <Route path="/">
+            <select
+              onChange={e => this.setState({ indexName: e.target.value })}
+            >
+              <option value="best-buy">Relevance</option>
+              <option value="best-buy_price_desc">Price Descending</option>
+              <option value="best-buy-price_asc">Price Ascending</option>
+              <option value="best-buy_perso_nick">Personalized for me</option>
+            </select>
+            <InstantSearch
+              searchClient={algoliasearch(
+                'latency',
+                '6be0576ff61c053d5f9a3225e2a90f76'
+              )}
+              indexName="instant_search_demo_query_suggestions"
+            >
+              <Configure hitsPerPage={5} />
+              <Autocomplete
+                onSuggestionSelected={this.onSuggestionSelected}
+                onSuggestionCleared={this.onSuggestionCleared}
+              />
+            </InstantSearch>
+
+            <InstantSearch searchClient={searchClient} indexName={indexName}>
+              <Configure hitsPerPage={16} />
+              <VirtualSearchBox defaultRefinement={query} />
+              <VirtualRefinementList
+                attribute="categories"
+                defaultRefinement={categories}
+              />
+              <div className="left-panel">
+                <ClearRefinements />
+                <HierarchicalMenu
+                  attributes={[
+                    'hierarchicalCategories.lvl0',
+                    'hierarchicalCategories.lvl1',
+                    'hierarchicalCategories.lvl2',
+                    'hierarchicalCategories.lvl3',
+                  ]}
                 />
-                <div className="left-panel">
-                  <ClearRefinements />
-                  <HierarchicalMenu
-                    attributes={[
-                      'hierarchicalCategories.lvl0',
-                      'hierarchicalCategories.lvl1',
-                      'hierarchicalCategories.lvl2',
-                      'hierarchicalCategories.lvl3',
-                    ]}
-                  />
-                  <h2>Brand</h2>
-                  <RefinementList attribute="brand" />
-                  <h2>Type</h2>
-                  <RefinementList attribute="type" />
-                  <h2>Free Shipping</h2>
-                  <RefinementList attribute="free_shipping" />
-                  <h2>Star Rating</h2>
-                  <RatingMenu attribute="rating" />
-                </div>
-                <Hits hitComponent={HitWithInsights} />
-
-                <div className="pagination">
-                  <Pagination />
-                </div>
+                <h2>Brand</h2>
+                <RefinementList attribute="brand" />
+                <h2>Type</h2>
+                <RefinementList attribute="type" />
+                <h2>Free Shipping</h2>
+                <RefinementList attribute="free_shipping" />
+                <h2>Star Rating</h2>
+                <RatingMenu attribute="rating" />
               </div>
-            </div>
-            <Configure clickAnalytics />
-          </InstantSearch>
-        </Route>
-      </Switch>
-    </div>
-  );
+
+              <Hits hitComponent={Hit} />
+              <Configure clickAnalytics />
+            </InstantSearch>
+          </Route>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- instantsearch-app/src/components/Autocomplete.js
+++ instantsearch-app/src/components/Autocomplete.js
@@ -1,35 +1,145 @@
-import { autocomplete } from '@algolia/autocomplete-js';
-import React, { createElement, Fragment, useEffect, useRef } from 'react';
-import { render } from 'react-dom';
-import { connectAutoComplete } from 'react-instantsearch-core';
+import React, { useEffect, Component } from 'react';
+import PropTypes from 'prop-types';
+import { Highlight, connectAutoComplete } from 'react-instantsearch-dom';
+import AutoSuggest from 'react-autosuggest';
 
+class Autocomplete extends Component {
+  static propTypes = {
+    hits: PropTypes.arrayOf(PropTypes.object).isRequired,
+    currentRefinement: PropTypes.string.isRequired,
+    refine: PropTypes.func.isRequired,
+    onSuggestionSelected: PropTypes.func.isRequired,
+    onSuggestionCleared: PropTypes.func.isRequired,
+  };
 
-export const Autocomplete = (props) => {
-  const containerRef = useRef(null);
+  state = {
+    value: this.props.currentRefinement,
+  };
 
-  useEffect(() => {
-    if (!containerRef.current) {
-      return undefined;
+  isSuggestionHasCategories(suggestion) {
+    return (
+      suggestion.instant_search &&
+      suggestion.instant_search.facets &&
+      suggestion.instant_search.facets.exact_matches &&
+      suggestion.instant_search.facets.exact_matches.categories &&
+      suggestion.instant_search.facets.exact_matches.categories.length
+    );
+  }
+
+  normalizeSuggestionCategories(suggestions) {
+    return suggestions.map(suggestion => {
+      const context = suggestion.instant_search || {};
+      const facets = context.facets || {};
+      const matches = facets.exact_matches || {};
+      const categories = matches.categories || [];
+
+      return {
+        ...suggestion,
+        // eslint-disable-next-line
+        instant_search: {
+          ...context,
+          facets: {
+            ...facets,
+            // eslint-disable-next-line
+            exact_matches: {
+              ...matches,
+              categories,
+            },
+          },
+        },
+      };
+    });
+  }
+
+  createMostRelevantSuggestionForAllCategories(suggestion) {
+    return {
+      ...suggestion,
+      // eslint-disable-next-line
+      instant_search: {
+        ...suggestion.instant_search,
+        facets: {
+          ...suggestion.instant_search.facets,
+          // eslint-disable-next-line
+          exact_matches: {
+            ...suggestion.instant_search.facets.exact_matches,
+            categories: [{ value: 'ALL_CATEGORIES' }],
+          },
+        },
+      },
+    };
+  }
+
+  onChange = (_, { newValue }) => {
+    if (!newValue) {
+      this.props.onSuggestionCleared();
     }
 
-    const search = autocomplete({
-      container: containerRef.current,
-      renderer: { createElement, Fragment },
-      render({ children }, root) {
-        render(children, root);
-      },
-      ...props,
+    this.setState({
+      value: newValue,
     });
+  };
 
-    return () => {
-      search.destroy();
+  onSuggestionsFetchRequested = ({ value }) => {
+    console.log(this.props.refine);
+    this.props.refine(value);
+  };
+
+  onSuggestionsClearRequested = () => {
+    console.log(this.props.refine);
+    this.props.refine();
+  };
+
+  getSuggestionValue(hit) {
+    return hit.query;
+  }
+
+  renderSuggestion(hit) {
+    const [category] = hit.instant_search.facets.exact_matches.categories;
+
+    return (
+      <span>
+        <Highlight attribute="query" hit={hit} tagName="mark" />
+        {category && (
+          <i>
+            {' '}
+            in{' '}
+            {category.value === 'ALL_CATEGORIES'
+              ? 'All categories'
+              : category.value}
+          </i>
+        )}
+      </span>
+    );
+  }
+
+  render() {
+    const { hits, onSuggestionSelected } = this.props;
+    const { value } = this.state;
+
+    const inputProps = {
+      placeholder: 'Search for a product...',
+      onChange: this.onChange,
+      value,
     };
-  }, [props]);
 
-  return <div ref={containerRef}></div>;
-};
+    const suggestions = this.normalizeSuggestionCategories(hits);
+    const suggestionsWithAllCategories =
+      suggestions[0] && this.isSuggestionHasCategories(suggestions[0])
+        ? [this.createMostRelevantSuggestionForAllCategories(suggestions[0])]
+        : [];
 
-export const CustomAutocomplete = connectAutoComplete(Autocomplete);
+    return (
+      <AutoSuggest
+        suggestions={[...suggestionsWithAllCategories, ...suggestions]}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        onSuggestionSelected={onSuggestionSelected}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={inputProps}
+      />
+    );
+  }
+}
 
-export default CustomAutocomplete;
-
+export default connectAutoComplete(Autocomplete);
