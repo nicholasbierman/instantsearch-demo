f05ec7babef7dbe0041428725ee62b8fc1bbf4da
Nick Bierman <biermannick83@gmail.com>
2021-08-04 10:46:35 -0500

    Updates

---
+++ precache-manifest.455d5e6ff7f6a86ce09350567fb14f18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db6924fbf5b7d4d52f0",
+    "url": "/instantsearch-demo/static/css/main.7341dd92.chunk.css"
+  },
+  {
+    "revision": "4db6924fbf5b7d4d52f0",
+    "url": "/instantsearch-demo/static/js/main.4db6924f.chunk.js"
+  },
+  {
+    "revision": "4675edc88bb36967f5b8",
+    "url": "/instantsearch-demo/static/css/1.d7588d75.chunk.css"
+  },
+  {
+    "revision": "4675edc88bb36967f5b8",
+    "url": "/instantsearch-demo/static/js/1.4675edc8.chunk.js"
+  },
+  {
+    "revision": "9d4c0be65dfcd9572e28",
+    "url": "/instantsearch-demo/static/js/runtime~main.9d4c0be6.js"
+  },
+  {
+    "revision": "068a241dbe8cc4dc43a04e95a3972f60",
+    "url": "/instantsearch-demo/index.html"
+  }
+];
