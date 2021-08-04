e8581522d60edb56ca1b7bcf2498fefb559f8ba4
Nick Bierman <biermannick83@gmail.com>
2021-08-04 13:51:59 -0500

    update imports on root

--- instantsearch-app/src/index.js
+++ instantsearch-app/src/index.js
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import configureStore from './store';
 import { BrowserRouter } from 'react-router-dom';
-
-const store = configureStore();
-
-window.store = store;
 
 function Root() {
   return (
